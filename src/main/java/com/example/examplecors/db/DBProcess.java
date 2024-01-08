f8d7067ae4033a012ac0d494dc842bdd272fce54
Parami <paramiashinsana@gmail.com>
2024-01-08 23:24:02 +0530

    create the DBProcess

--- src/main/java/com/example/examplecors/db/DBProcess.java
+++ src/main/java/com/example/examplecors/db/DBProcess.java
@@ -1,4 +1,27 @@
 package com.example.examplecors.db;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DBProcess {
+    private static final String SAVE_ITEM_DATA = "INSERT INTO ITEMS (CODE,DESCRIPTION,QTY,PRICE) VALUES (?,?,?,?)";
+
+    public void saveItemOne(ItemDTO items, Connection connection){
+        try {
+            var ps = connection.prepareStatement(SAVE_ITEM_DATA);
+            ps.setString(1, customItemId);
+            ps.setString(2, items.getDescription());
+            ps.setInt(3, items.getQty());
+            ps.setDouble(4, items.getUnitPrice());
+
+            if (ps.executeUpdate() != 0) {
+                System.out.println("Data saved");
+            } else {
+                System.out.println("Failed to save");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
