073e2345373513a82f8280d003e6a0926a80ef62
Parami <paramiashinsana@gmail.com>
2024-01-09 14:47:39 +0530

    create the connection

--- src/main/java/com/example/examplecors/api/Item.java
+++ src/main/java/com/example/examplecors/api/Item.java
@@ -1,7 +1,20 @@
 package com.example.examplecors.api;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import java.sql.Connection;
 
+@WebServlet(name = "item", urlPatterns = "/item",
+        initParams = {
+                @WebInitParam(name = "db-user", value = "root"),
+                @WebInitParam(name = "db-pw", value = "1234"),
+                @WebInitParam(name = "db-url", value = "jdbc:mysql://localhost:3306/pos24?createDatabaseIfNotExist=true"),
+                @WebInitParam(name = "db-class", value = "com.mysql.cj.jdbc.Driver"),
+        },
+        loadOnStartup = 10
+)
 public class Item extends HttpServlet {
+    Connection connection;
 
 }
